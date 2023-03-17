28d6179bb4aeb073b2c0660bcbc785fd844d8c79
rcammat <mateosraimundo@gmail.com>
2023-03-17 01:12:02 +0100

    Firts Commit

---
+++ SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository<TModelo> : IGenericRepository<TModelo> where TModelo : class
+    {
+        private readonly DbventaContext _dbContext;
+
+        public GenericRepository(DbventaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TModelo> Obtener(Expression<Func<TModelo, bool>> filtro)
+        {
+            try
+            {
+                TModelo modelo = await _dbContext.Set<TModelo>().FirstOrDefaultAsync(filtro);
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<TModelo> Crear(TModelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<TModelo>().Add(modelo);
+                await _dbContext.SaveChangesAsync();
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(TModelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<TModelo>().Update(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminr(TModelo modelo)
+        {
+            try
+            {
+                _dbContext.Set<TModelo>().Remove(modelo);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<IQueryable<TModelo>> Consultar(Expression<Func<TModelo, bool>>? filtro = null)
+        {
+            try
+            {
+                IQueryable<TModelo> queryModelo = filtro == null ? _dbContext.Set<TModelo>(): _dbContext.Set<TModelo>().Where(filtro);
+                return queryModelo;
+
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
